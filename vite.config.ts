eddacab317d50b27998026d4272b08aa4125b6a8
aldofahrezy <aldofhrzy@gmail.com>
2026-03-03 15:30:23 +0700

    chore(config): configure vite reverse proxy for microservices
    Set up proxy rules in Vite to route API requests to their respective backend microservices (Auth, Catalogue, Auction, Wallet) and bypass CORS restrictions during local development.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1/auth': {
+        target: 'http://localhost:8081', // Port Service Auth
+        changeOrigin: true,
+      },
+      '/api/v1/catalogue': {
+        target: 'http://localhost:8082', // Port Service Catalogue
+        changeOrigin: true,
+      },
+      '/api/v1/auctions': {
+        target: 'http://localhost:8080', // Port Service Auction
+        changeOrigin: true,
+      },
+      '/api/v1/wallet': {
+        target: 'http://localhost:8083', // Port Service Wallet
+        changeOrigin: true,
+      }
+    }
+  }
 })
