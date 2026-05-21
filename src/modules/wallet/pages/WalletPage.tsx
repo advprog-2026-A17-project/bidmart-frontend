6c0707336a3a1abbae0a0db70a3c9ef9fae198be
aldofahrezy <aldofhrzy@gmail.com>
2026-05-21 16:15:29 +0700

    Remove duplicate showSuccess callback in WalletPage.

--- src/modules/wallet/pages/WalletPage.tsx
+++ src/modules/wallet/pages/WalletPage.tsx
@@ -146,12 +146,6 @@ const WalletPage: React.FC = () => {
         setTimeout(() => setSuccess(null), 3000);
     }, []);
 
-    const showSuccess = useCallback((msg: string) => {
-        setSuccess(msg);
-        setError(null);
-        setTimeout(() => setSuccess(null), 3000);
-    }, []);
-
     useEffect(() => {
         fetchWallet();
     }, [fetchWallet]);
